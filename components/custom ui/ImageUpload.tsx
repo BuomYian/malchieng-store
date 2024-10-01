366712b08576207087ce3394f6f862b2f5058edb
Buomkuoth Yian <buom.nyajal@gmail.com>
2024-10-02 00:08:37 +0300

    Implemented collections page

---
+++ components/custom ui/ImageUpload.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { CldUploadWidget } from "next-cloudinary";
+import { Button } from "../ui/button";
+import { Plus, Trash } from "lucide-react";
+import Image from "next/image";
+
+interface ImageUploadProps {
+  value: string[];
+  onChange: (value: string[]) => void;
+  onRemove: (value: string[]) => void;
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  onChange,
+  onRemove,
+  value,
+}) => {
+  // Use onSuccess instead of onUpload if onUpload is deprecated
+  const handleUploadSuccess = (result: any) => {
+    onChange(result.info.secure_url);
+  };
+
+  return (
+    <div>
+      <div className="mb-4 flex flex-wrap items-center gap-4">
+        {value.map((url) => (
+          <div className="relative w-[200px] h-[200px]" key={url}>
+            <div className="absolute top-0 right-0 z-10">
+              <Button
+                onClick={() => onRemove(url)}
+                size="sm"
+                className="bg-red-1 text-white"
+              >
+                <Trash className="h-4 w-4" />
+              </Button>
+            </div>
+            <Image
+              src={url}
+              alt="collection"
+              className="object-cover rounded-lg"
+              fill
+            />
+          </div>
+        ))}
+      </div>
+      <CldUploadWidget
+        uploadPreset="ekh7ucq9"
+        onSuccess={handleUploadSuccess} // Update this to the new event handler
+      >
+        {({ open }) => {
+          return (
+            <Button onClick={() => open()} className="bg-grey-1 text-white">
+              <Plus className="h-4 w-4 mr-2" />
+              Upload Image
+            </Button>
+          );
+        }}
+      </CldUploadWidget>
+    </div>
+  );
+};
+
+export default ImageUpload;
