77a9d3b7074b732444fae05695025b48e58cebc0
Buomkuoth Yian <buom.nyajal@gmail.com>
2024-10-03 21:58:49 +0300

    Implemented edit collection route

--- app/api/collections/[collectionId]/route.ts
+++ app/api/collections/[collectionId]/route.ts
@@ -3,6 +3,68 @@ import { connectToDB } from "@/lib/mongoDB";
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
+export const GET = async (
+  rea: NextRequest,
+  { params }: { params: { collectionId: string } }
+) => {
+  try {
+    await connectToDB();
+    const collection = await Collection.findById(params.collectionId);
+
+    if (!collection) {
+      return new NextResponse(
+        JSON.stringify({ message: "Collection not found" }),
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(collection, { status: 200 });
+  } catch (error) {
+    console.log("[collectionId_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+};
+
+export const POST = async (
+  req: NextRequest,
+  { params }: { params: { collectionId: string } }
+) => {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("unauthorized", { status: 401 });
+    }
+
+    await connectToDB();
+
+    let collection = await Collection.findById(params.collectionId);
+
+    if (!collection) {
+      return new NextResponse("Collection not found", { status: 401 });
+    }
+
+    const { title, description, image } = await req.json();
+
+    if (!title || !image) {
+      return new NextResponse("Missing required fields", { status: 400 });
+    }
+
+    collection = await Collection.findByIdAndUpdate(
+      params.collectionId,
+      { title, description, image },
+      { new: true }
+    );
+
+    await collection.save();
+
+    return NextResponse.json(collection, { status: 200 });
+  } catch (error) {
+    console.log("[collectionId_POST]", error);
+    return new NextResponse("Internal server error", { status: 500 });
+  }
+};
+
 export const DELETE = async (
   req: NextRequest,
   { params }: { params: { collectionId: string } }
